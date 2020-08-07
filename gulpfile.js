92ea7171f09e57cd0786e0514522b5a3851c6c31
root <root@DESKTOP-G1L1POL.trafficsafetystore.local>
2020-08-07 15:42:31 -0400

    Initialy commit should have done this earlier but better late than never

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+
+function watch() {
+    browserSync.init({
+        server: {
+           baseDir: "./",
+           index: "./index.html"
+        }
+    });
+    gulp.watch('./*.html').on('change',browserSync.reload);
+    gulp.watch('./*.js').on('change', browserSync.reload);
+    gulp.watch('./*.css').on('change', browserSync.reload);
+}
+
+exports.watch = watch;
