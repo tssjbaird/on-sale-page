474ec5756567575d605f2f5c2a443d330ca53932
tss-awertheimer <root@DESKTOP-G1L1POL.trafficsafetystore.local>
2020-09-02 15:03:58 -0400

    pull request

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
+‘use strict’;
+
 const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+
+sass.compiler = require('node-sass');gulp.task('sass', function () {   return gulp.src('./scss/*.scss')
+   .pipe(concat('custom.scss'))
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('./dist/'));});
 
 function watch() {
     browserSync.init({
